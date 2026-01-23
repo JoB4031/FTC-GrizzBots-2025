b837603de0542dc3c62a230f6914e4fb2d13a608
MrChips10 <jason.fogiel@outlook.com>
2026-01-23 10:13:40 -0500

    Updates Tuned Fidget Tech

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Motors.java
@@ -25,9 +25,9 @@ public class Motors{
     public static double kF = 0.00025;
     public int spinPosition;
     public final double[] spinPositions = {
-            0.03,0.06,0.1,0.14,0.17,0.21,0.25,0.28,0.32,0.36,
-            0.4,0.43,0.47,0.51,0.55,0.58,0.62,0.66,0.7,0.73,
-            0.77,0.8,0.83,0.87,0.91,0.94,0.98
+            0,0.04,0.08,0.12,0.15,0.19,0.22,0.26,0.3,0.34,0.38,0.32,0.41,
+            0.44,0.48,0.52,0.56,0.61,0.64,0.68,0.72,0.75,0.83,
+            0.86,0.9,0.93,0.97
     };
     public boolean ableToShoot;
 
@@ -37,7 +37,7 @@ public class Motors{
         ballEjector = hardwareMap.get(Servo.class, "ballEjector");
         fidgetTech = hardwareMap.get(Servo.class, "spinIndexer");
 
-        spinPosition = 13;
+        spinPosition = 15;
 
         rightFlywheel = hardwareMap.get(DcMotorEx.class, "rightFlywheel");
         rightFlywheel.setDirection(DcMotorEx.Direction.FORWARD);
@@ -71,13 +71,13 @@ public class Motors{
         // Sets the fidget tech position to pick-up or fire based on whether the intake is on or off
         if(intake.getPower() > 0) {
             if (spinPosition % 2 == 1) {
-                if (spinPosition >= 13) {
+                if (spinPosition >= 15) {
                     spinPosition -= 1;
                 } else spinPosition += 1;
             }
         } else {
             if(spinPosition % 2 == 0){
-                if (spinPosition >= 13) {
+                if (spinPosition >= 15) {
                     spinPosition -= 1;
                 } else spinPosition += 1;
             }
