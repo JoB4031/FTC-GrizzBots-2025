3947b742429724c2f4409e6d2118344a513e0040
MrChips10 <jason.fogiel@outlook.com>
2026-02-25 14:52:26 -0500

    Updates the Fidget Tech Positions for the new Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/motors/FidgetTech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/motors/FidgetTech.java
@@ -10,10 +10,9 @@ public class FidgetTech {
     private Servo indexer;
     private static int snapPoint = 15;
 
-    private final double[] positions = {
-            0.04, 0.075, 0.115, 0.15, 0.185, 0.225, 0.26, 0.3, 0.33, 0.37,
-            0.405, 0.445, 0.485, 0.525, 0.565, 0.605, 0.645, 0.675, 0.715,
-            0.755, 0.79, 0.83, 0.86, 0.9, 0.94, 0.975, 1
+    private final double[] positions = {0.0, 0.03, 0.07, 0.105, 0.145, 0.18, 0.22, 0.26,
+            0.295, 0.325, 0.365, 0.405, 0.445, 0.485, 0.525, 0.565, 0.6, 0.64, 0.68,
+            0.715, 0.755, 0.785, 0.825, 0.855, 0.89, 0.925, 0.96, 0.995
     };
     public static IntakeSensor.detectedColor[] artifactsLoaded = {
             IntakeSensor.detectedColor.NONE, IntakeSensor.detectedColor.NONE, IntakeSensor.detectedColor.NONE
